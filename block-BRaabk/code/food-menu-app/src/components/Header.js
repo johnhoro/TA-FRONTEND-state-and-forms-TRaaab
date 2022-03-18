f6014f6bd49472e4d7f5ef2bc15706d1cf9df638
john horo <johnhoro125@gmail.com>
2022-03-18 11:47:06 +0530

    Food-menu

--- block-BRaabk/code/food-menu-app/src/components/Header.js
+++ block-BRaabk/code/food-menu-app/src/components/Header.js
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <header className="mt-10 p-5 flex flex-col items-center">
-      <h1 className="text-3xl font-bold">Our Menu</h1>
-      <hr className="w-20 border-2 border-red-500" />
+    <header>
+      <h1>Our Menu</h1>
+      <hr />
     </header>
   );
 }
