ac1e4d15f72698fbd0eec5ca332703d3a93dce16
john horo <johnhoro125@gmail.com>
2022-03-18 10:29:50 +0530

    Food-menu

---
+++ block-BRaabk/code/food-menu-app/src/components/Tag.js
@@ -0,0 +1,59 @@
+import React from "react";
+import data from "../data.json";
+import Menu from "./Menu";
+
+class Tag extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTag: "all",
+    };
+  }
+  render() {
+    let allCategories = data.map((food) => food.category);
+    // console.log(loadash.unique(allCategories));
+    console.log(allCategories);
+    let categories = allCategories.filter(
+      (category, i, array) => array.indexOf(category) === i
+    );
+    console.log(categories);
+    let allProducts;
+
+    if (this.state.activeTag === "all") {
+      allProducts = data;
+    } else {
+      allProducts = data.filter(
+        (data) => this.state.activeTag === data.category
+      );
+    }
+    return (
+      <div className="border">
+        <header className="flex justify-center">
+          {categories.map((category, i) => {
+            console.log(category);
+            return (
+              <>
+                <button
+                  className={this.state.activeTag === category ? "active" : ""}
+                  onClick={() => {
+                    this.setState({
+                      activeTag: category,
+                    });
+                  }}
+                >
+                  {category.toUpperCase()}
+                </button>
+              </>
+            );
+          })}
+        </header>
+        <main className="p-12">
+          <section className="flex justify-between flex-wrap">
+            <Menu allProducts={allProducts} />
+          </section>
+        </main>
+      </div>
+    );
+  }
+}
+export default Tag;
